e00cd35e7ae6861c8fe6e68dbf2e37de23071484
Your Name <you@example.com>
2022-01-14 04:36:30 +0000

    kind

--- kind-scripts/prepare-docker-image.sh
+++ kind-scripts/prepare-docker-image.sh
@@ -2,4 +2,6 @@ docker pull kindest/node:v1.21.1
 docker save kindest/node:v1.21.1 -o kindest-node.tar
 
 docker load -i kindest-node.tar
+docker tag kindest/node:v1.21.1 infra-harbor.lab.pcfdemo.net/tkg/kind/node:v1.21.2_vmware.1
+docker push infra-harbor.lab.pcfdemo.net/tkg/kind/node:v1.21.2_vmware.1
 ##kind create cluster --image=kindest/node:v1.21.1 
