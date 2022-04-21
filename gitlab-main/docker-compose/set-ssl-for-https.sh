193f6291345c8ce40ba8a29b61d89faa0fe29e2e
Your Name <you@example.com>
2022-04-21 07:32:41 +0000

    fix gitlab

--- gitlab-main/docker-compose/set-ssl-for-https.sh
+++ gitlab-main/docker-compose/set-ssl-for-https.sh
@@ -1,6 +1,8 @@
+#!/bin/bash
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 TARGET="/data/gitlab-data/config/ssl"
 rm -rf $TARGET
 mkdir -p $TARGET
-ln  /data/gitlab-main/generate-self-signed-cert/domain.crt $TARGET/gitlab2.lab.pcfdemo.net.crt
-ln  /data/gitlab-main/generate-self-signed-cert/domain.key $TARGET/gitlab2.lab.pcfdemo.net.key
+ln  $SCRIPT_DIR/generate-self-signed-cert/domain.crt $TARGET/gitlab2.lab.pcfdemo.net.crt
+ln  $SCRIPT_DIR/generate-self-signed-cert/domain.key $TARGET/gitlab2.lab.pcfdemo.net.key
 ls -al $TARGET
